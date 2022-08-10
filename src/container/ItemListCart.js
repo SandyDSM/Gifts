b54dd1540b1ad1785c2264bdc60b74a229df5959
SandyDSM <sad.zoe@gmail.com>
2022-08-09 19:21:05 -0500

    Desafío: Item Colletion II

--- src/container/ItemListCart.js
+++ src/container/ItemListCart.js
@@ -4,24 +4,67 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../sass/custom.css";
 import ItemCart from "../components/ItemCart";
+import { collection, doc, increment, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
+import {db} from "../utils/firebaseConfig"
+import { async } from "@firebase/util";
 
 const ItemListCart = () => {
   const test = useContext(CartContext);
   console.log(test.cartList)
+  const createOrder = () =>{
+    let itemsForDB = test.cartList.map(item=>({
+      id:item.id,
+      title: item.name,
+      price: item.price,
+      qty: item.quantity
+    }))
+    let order = {
+      buyer:{
+        email: "sandy@gmail.com",
+        name: "Sandy de los Santos",
+        phone: "5512345678"
+      },
+      date: serverTimestamp(),
+      items: itemsForDB,
+      total:test.totalPrice()
+    }
+    console.log(order)
+
+    const createOrderInDb = async () =>{
+      const newOrderRef = doc(collection(db, "orders"))
+      await setDoc(newOrderRef, order)
+      return newOrderRef
+    }
+
+    createOrderInDb()
+      .then(result => alert('tu orden ha sido creada. ID de orden:' +result.id))
+      .catch(err => console.log(err))
+
+    test.cartList.forEach(async(item) =>{
+      const itemRef = doc(db, "products", item.id)
+      await updateDoc(itemRef,{
+        stock: increment(-item.quantity)
+      })
+    })
+    
+    test.clear();
+  }
+
   if(test.cartList.length > 0){
     return (
       <div>
         <h1 className="text-primary text-center mt-5">Regalos en tu carrito</h1>
-        <div className="row justify-content-end">
-          <Link to='/' className="col-6 btn btn-info text-light col-md-3 my-3 me-4">Ir por más Regalos</Link>
+        <div className="row justify-content-between">
+          <Link to='/' className="col-4 btn btn-outline-info col-md-3 m-3"onClick={()=>test.clear()}>Vaciar carrito</Link>
+          <Link to='/' className="col-4 btn btn-info text-light col-md-3 my-3 me-4">Más Regalos</Link>
         </div>
         {
           test.cartList.map(product => <ItemCart key={product.id} product={product}/>)
         }
-        <Link to='/' className="col-6 btn btn-outline-info col-md-3 m-3"onClick={()=>test.clear()}>Vaciar carrito</Link>
+        
         <div className="d-flex flex-column align-items-end">
           <p className="col-12 display-6 text-center text-md-end">Total: ${test.totalPrice().toFixed(2)}</p>
-          <Link to='#' className="col-12 btn btn-info text-light col-md-3 my-3">Finalizar compra</Link>
+          <button onClick={createOrder} className="col-12 btn btn-info text-light col-md-3 my-3">Finalizar compra</button>
         </div>
       </div>
     );
